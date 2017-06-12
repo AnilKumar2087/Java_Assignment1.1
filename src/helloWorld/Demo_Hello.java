a794b6f901c5aacbd9bd50d23198d5181d454e81
AnilKumar2087 <a.anil.adunuri@gmail.com>
2017-06-12 22:33:07 +0530

    assignmnet

---
+++ src/helloWorld/Demo_Hello.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class Demo_Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloo world from Anil");
+
+
+	}
+
+}
